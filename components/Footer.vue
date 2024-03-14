cb368cc2791a97c47a8ca7570efefc6aad06f365
kebaHailu <kibromhailu.5513@gmail.com>
2024-03-14 13:47:54 +0300

    Create the structure of the project

---
+++ components/Footer.vue
@@ -0,0 +1,83 @@
+
+<template>
+    <footer class="bg-green-900 px-8">
+
+
+        <div class="flex flex-wrap justify-around">
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">All Categories</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">About Us</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Back To Top</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Advertises</h2>
+            </div>
+        </div>
+
+        <div class="flex flex-wrap justify-between">
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Privacy Notices</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Help Center</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Home</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Recipes</h2>
+            </div>
+        </div>
+
+        <div class="flex flex-wrap justify-between">
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Ingredients</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Ingredients</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Ingredients</h2>
+            </div>
+            <div class="w-full md:w-1/4 py-2">
+                <h2 class="text-white">Ingredients</h2>
+            </div>
+            <div class="footer-content py-3  flex justify-center">
+                <a href="#" class="text-white mr-4">
+                    <Icon name="ic:baseline-facebook" size="30" color="white" />
+                </a>
+                <a href="#" class="text-white mr-4">
+                    <Icon name="lets-icons:insta-fill" size="30" color="white" />
+                </a>
+                <a href="#" class="text-white mr-4">
+                    <Icon name="mdi:twitter" size="30" color="white" />
+                </a>
+                <a href="#" class="text-white">
+                    <Icon name="mdi:youtube" size="30" color="white" />
+                    <UColorModeButton size="sm" />
+                </a>
+            </div>
+            <div>
+
+            </div>
+            <div class="justify-center">
+                <div class="w-full py-2 px-8">
+                    <h2 class="text-white">© 2024 Ethio Food Inc. All rights reserved.</h2>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+
+
+<style>
+.footer-content {
+    height: 10vh;
+
+}
+</style>
